90d66635c9783a7269d2439646e51721744c78c2
yaparlachandramohan <yaparlachandramohan@gmail.com>
2017-01-20 16:02:06 +0530

    Added custom rake task and modified the api, deploy and routes file

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,13 +54,3 @@ set :puma_preload_app, false
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
-desc 'Runs rake db:seed'
-task :seed => [:set_rails_env] do
-  on primary fetch(:migration_role) do
-    within release_path do
-      with rails_env: fetch(:rails_env) do
-        execute :rake, "db:seed"
-      end
-    end
-  end
-end
