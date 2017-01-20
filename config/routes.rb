c2354142d5621cd925320163c318ec8690d39a53
yaparlachandramohan <yaparlachandramohan@gmail.com>
2017-01-20 16:06:26 +0530

    Changed the get method to post in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
  
-  match '/api/save_pdf' => 'api#save_pdf', via:[:get]
+  match '/api/save_pdf' => 'api#save_pdf', via:[:post]
   root 'pdfjobs#index'
 
   # Example of regular route:
