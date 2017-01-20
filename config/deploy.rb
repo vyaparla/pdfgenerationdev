f958cd414c714ed00134b6fad49f24a3e631407b
yaparlachandramohan <yaparlachandramohan@gmail.com>
2017-01-20 14:52:37 +0530

    Modified the deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "3.7.1"
 set :application, "pdfgenerationdev"
 set :repo_url, 'git@github.com:vyaparla/pdfgenerationdev.git' # Edit this to match your repository
 set :branch, :master
-set :deploy_to, '/home/pdfgenerationdev'
+set :deploy_to, '/home//pdfgenerationdev'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
