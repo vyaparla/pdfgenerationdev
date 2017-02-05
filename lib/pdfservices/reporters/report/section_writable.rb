3f453075b227f62108e0e1a6590e67d77bbf5d77
yaparlachandramohan <yaparlachandramohan@gmail.com>
2017-02-05 20:24:58 +0530

    Modified the Section Writable file

--- lib/pdfservices/reporters/report/section_writable.rb
+++ lib/pdfservices/reporters/report/section_writable.rb
@@ -10,14 +10,12 @@ module Report
   private
 
     def records
-      @records ||= @job.building_records(@building)
+      @records ||= @job.building_records(@building, @job.u_service_id)
       #@records = Lsspdfasset.where(:u_building => building, :u_service_id => job.u_service_id)
-      #Rails.logger.debug("Section Writable Recrds: #{@records.inspect}")
     end
 
     def building_section
       @building_section = @building
     end
-
   end
 end
