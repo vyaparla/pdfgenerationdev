3f07cd363b470a0083e03ec2e524a0a058e13216
Venkata  Mohan <vyaparla@vsoftconsulting.com>
2017-04-27 14:26:59 +0530

    Modified the date format in FireDoor Inspection Service Type

--- app/pdfservices/reporters/report/door_inspection_report/table_page.rb
+++ app/pdfservices/reporters/report/door_inspection_report/table_page.rb
@@ -40,7 +40,7 @@ module DoorInspectionReport
       ]
       @records.each do |record|
         @firedoor_deficiency_codes = FiredoorDeficiency.where(:firedoor_service_sysid => record.u_service_id, :firedoor_asset_sysid => record.u_asset_id).collect { |w| w.firedoor_deficiencies_code }.join(", ")        
-        inspection_data << [record.u_inspected_on, record.u_floor, record.u_tag, record.u_fire_rating, record.u_location_desc, record.u_door_type, record.u_door_inspection_result, @firedoor_deficiency_codes]
+        inspection_data << [record.u_inspected_on.strftime('%m/%d/%Y'), record.u_floor, record.u_tag, record.u_fire_rating, record.u_location_desc, record.u_door_type, record.u_door_inspection_result, @firedoor_deficiency_codes]
       end
       #create the table & write it into the PDF
       pdf.font_size 10
