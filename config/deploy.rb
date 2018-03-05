f45b5b65b4ea59d272dbde4ef9f00c807c0074c1
Venkata  Mohan <vyaparla@vsoftconsulting.com>
2018-03-05 22:45:26 +0530

    LSS PDF Generation development commit2

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@
 
 set :application, "pdfgenerationdev"
 set :repo_url, 'git@github.com:vyaparla/pdfgenerationdev.git' # Edit this to match your repository
-set :branch, :master
+set :branch, :development
 set :deploy_to, '/home/deploy/pdfgenerationdev'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
@@ -13,7 +13,7 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 set :keep_releases, 5
 set :rvm_type, :user
 set :rvm_ruby_version, '2.2.2' # Edit this if you are using MRI Ruby
-set :stage, :staging
+set :stage, ["staging", "production"]
 
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 #set :puma_bind,  "unix:///#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
@@ -24,7 +24,7 @@ set :puma_conf, "#{shared_path}/puma.rb"
 set :puma_access_log, "#{shared_path}/log/puma_error.log"
 set :puma_error_log, "#{shared_path}/log/puma_access.log"
 set :puma_role, :app
-set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'development'))
 set :puma_threads, [4, 8]
 set :puma_workers, 2
 set :puma_worker_timeout, nil
@@ -32,7 +32,7 @@ set :puma_init_active_record, true
 set :puma_preload_app, true
 set :assets_roles, [:web, :app]
 
-set :console_env, :production
+set :console_env, :development
 
 after "deploy:restart", "deploy:cleanup"
 
@@ -53,8 +53,8 @@ namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
+      unless `git rev-parse HEAD` == `git rev-parse origin/development`
+        puts "WARNING: HEAD is not the same as origin/development"
         puts "Run `git push` to sync changes."
         exit
       end
