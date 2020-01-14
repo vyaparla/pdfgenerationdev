9dded689d77fd9db1ebfd90925ca0812e9501f28
Venkata  Mohan <vyaparla@vsoftconsulting.com>
2020-01-14 15:13:08 +0530

    Routing to development branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,8 +5,8 @@
 
 set :application, "pdfgenerationdev"
 set :repo_url, 'git@github.com:vyaparla/pdfgenerationdev.git' # Edit this to match your repository
-#set :branch, :development # This branch is used for production and uat
-set :branch, :vsoft_qa # This branch is used for vsoft qa 
+set :branch, :development # This branch is used for production and uat
+#set :branch, :vsoft_qa # This branch is used for vsoft qa 
 set :deploy_to, '/home/deploy/pdfgenerationdev'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
@@ -65,8 +65,8 @@ namespace :deploy do
     on roles(:app) do
      # unless `git rev-parse HEAD` == `git rev-parse origin/development` # For production and UAT
      #   puts "WARNING: HEAD is not the same as origin/development" # For production and UAT
-     unless `git rev-parse HEAD` == `git rev-parse origin/vsoft_qa` # For VSoft QA
-        puts "WARNING: HEAD is not the same as origin/vsoft_qa" # For VSoft QA
+     unless `git rev-parse HEAD` == `git rev-parse origin/development` # For VSoft QA
+        puts "WARNING: HEAD is not the same as origin/development" # For VSoft QA
         puts "Run `git push` to sync changes."
         exit
       end
