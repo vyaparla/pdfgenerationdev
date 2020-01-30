21c1eee472efa9d5fa567a31bb72f27fdf2555bb
Nisha <nganesh@vsoftconsulting.com>
2020-01-30 15:24:30 +0530

    Project Completion Report

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -508,7 +508,8 @@ class ApiController < ApplicationController
       request_body_map = {
         "sys_id" => "#{@project_completion.m_service_sysid}",
         "project_completion_timestamp" => "#{@project_completion.m_date.strftime("%m-%d-%Y-%I-%M-%p")}",
-        "pdf_url" => "ec2-54-165-215-71.compute-1.amazonaws.com/api/download_project_completion_pdf_report?service_sysid=#{@project_completion.id}",
+        "pdf_url" => "ec2-54-196-89-5.compute-1.amazonaws.com/api/download_project_completion_pdf_report?service_sysid=#{@project_completion.id}",
+       # "pdf_url" => "ec2-54-165-215-71.compute-1.amazonaws.com/api/download_project_completion_pdf_report?service_sysid=#{@project_completion.id}",
       }.to_json
       
       begin
