9ce81d2096820ba56dff3da9b50d082b40ba703a
Manjula KR <rajmanjula2006@gmail.com>
2020-03-11 16:37:29 +0530

    watermark firestop asset list table

--- app/pdfservices/reporters/firestop_facility_reporter.rb
+++ app/pdfservices/reporters/firestop_facility_reporter.rb
@@ -2,7 +2,7 @@ class FirestopFacilityReporter < Reporter
 
   def comprehensive_report(job, model_name, address1, address2, csz, facility_type, tech, group_name, facility_name, facility_id, with_picture=true, report_type, watermark)
      FirestopComprehensiveReport::GraphGenerator.new(job).generate
-     generate(job.full_facilitywise_report_path(with_picture, model_name, report_type, watermark)) do |pdf|
+     generate(job.full_facilitywise_report_path(with_picture, model_name, report_type)) do |pdf|
        Report::CoverPage.new(job, model_name="Firestop Comprehensive", address1, address2, csz, facility_name, tech, watermark).write(pdf)
        FirestopComprehensiveReport::SummaryPage.new(job, tech, watermark).write(pdf)
        job.comprehensive_buildings(facility_id).each do |b|
