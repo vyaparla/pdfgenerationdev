f88e4ed8ae459fdad5b570ccf6217f8e18eca349
Venkata  Mohan <vyaparla@vsoftconsulting.com>
2020-03-27 20:24:24 +0530

    lp-746 fix-5

--- app/pdfservices/reporters/report/damper_statement_report/graph_generator.rb
+++ app/pdfservices/reporters/report/damper_statement_report/graph_generator.rb
@@ -75,7 +75,7 @@ module DamperStatementReport
         @dr_result_graph_count += value
       end 
 
-      puts "***#{@dr_resultRecords}***#{@dr_result_graph_count}*********"
+      puts "******#{@dr_resultRecords}***#{@dr_result_graph_count}*********"
 
       @dr_resultRecords.each do |key1, value1|
         if key1 == "Pass"
