8d5b994233bed0225096ce94a4106fcf9cb7a21d
Nisha <nganesh@vsoftconsulting.com>
2020-04-03 12:36:27 +0530

    LP-714 & LP-740 - Damper Repair Report

--- lib/pdfservices/reporters/report/repair_summary_drawable.rb
+++ lib/pdfservices/reporters/report/repair_summary_drawable.rb
@@ -48,47 +48,28 @@ module Report
     end
     
     def summary_table_data
-     @buildingInfo = Lsspdfasset.select(:u_building, :u_floor, :u_type, :u_other_floor).where(:u_service_id => @owner.u_service_id, :u_building => @building, :u_delete => false).where.not(u_type: "").group(["u_building", "u_floor", "u_type", "u_other_floor"]).order(:u_floor).count(:u_type)
-
-     # @buildingInfo = Lsspdfasset.select(:u_building, :u_floor, :u_type).where(:u_service_id => @owner.u_service_id, :u_building => @building, :u_delete => false).where.not(u_type: "").group(["u_building", "u_floor", "u_type"]).order(:u_floor).count(:u_type)
+      @buildingInfo = Lsspdfasset.select(:u_building, :u_floor, :u_type, :u_other_floor).where(:u_service_id => @owner.u_service_id, :u_building => @building, :u_delete => false).where.not(u_type: "").group(["u_building", "u_floor", "u_type", "u_other_floor"]).order(:u_floor).count(:u_type)
+      building_floors = Lsspdfasset.select(:u_building, :u_floor, :u_type, :u_other_floor).where(:u_service_id => @owner.u_service_id, :u_building => @building, :u_delete => false).where.not(u_type: "").pluck(:u_floor)
+      building_other_floors = Lsspdfasset.select(:u_building, :u_floor, :u_type, :u_other_floor).where(:u_service_id => @owner.u_service_id, :u_building => @building, :u_delete => false).where.not(u_type: "").pluck(:u_other_floor)
       @floorInfo = []
-
       @buildingInfo.each do |key,value|
         floor_json = {}
         if @floorInfo.length == 0
-          
-          floor_json["building"] = key[0]
+           floor_json["building"] = key[0]
            floor_data = key[1] == "other" ? key[3] : key[1]
            floor_json["floor"] = floor_data
-         #  floor_json["floor"] = key[1]
-
-          if key[2] == "FSD"
-            floor_json["FSD"] = value
-            floor_json["FD"] = 0
-            floor_json["SD"] = 0
-          elsif key[2] == "FD"
-            floor_json["FSD"] = 0
-            floor_json["FD"] = value
-            floor_json["SD"] = 0
-          else
-            floor_json["FSD"] = 0
-            floor_json["FD"] = 0
-            floor_json["SD"] = value
-          end
-	  if  key[1] == "other" 
-	     @building_result = Lsspdfasset.select(:u_building, :u_other_floor, :u_dr_passed_post_repair).where(:u_service_id => @owner.u_service_id, :u_building => @building, :u_other_floor => key[3], :u_delete => false).where.not(u_type: "").group(["u_building", "u_other_floor", "u_dr_passed_post_repair"]).count(:u_dr_passed_post_repair)
-	  else 
-	
-
-
-
-         # @building_result = Lsspdfasset.select(:u_building, :u_floor, :u_dr_passed_post_repair, :u_other_floor).where(:u_service_id => @owner.u_service_id, :u_building => @building, :u_floor => key[1], :u_delete => false).where.not(u_type: "").group(["u_building", "u_floor", "u_dr_passed_post_repair", "u_other_floor"]).count(:u_dr_passed_post_repair)
-          @building_result = Lsspdfasset.select(:u_building, :u_floor, :u_dr_passed_post_repair).where(:u_service_id => @owner.u_service_id, :u_building => @building, :u_floor => key[1], :u_delete => false).where.not(u_type: "").group(["u_building", "u_floor", "u_dr_passed_post_repair"]).count(:u_dr_passed_post_repair)
-          end
+           for_type_calculate_non_integer_floor_values(building_floors, building_other_floors, floor_data, floor_json, key, value)
+	    calculate_non_integer_floor_values(building_floors, building_other_floors, floor_data, key)
           @building_result.each do |fstatus, fvalue|
-            if !floor_json.has_key?(fstatus[2])
-              floor_json[fstatus[2]] = fvalue
-            end
+	    if fstatus.class == String
+	       if !floor_json.has_key?(fstatus)
+                 floor_json[fstatus] = fvalue
+               end
+            else	
+              if !floor_json.has_key?(fstatus[2])
+                floor_json[fstatus[2]] = fvalue
+              end
+	    end
           end
 
           if !floor_json.has_key?("Pass")
@@ -102,49 +83,39 @@ module Report
           if !floor_json.has_key?("NA")
             floor_json["NA"] = 0
           end          
-          
           @floorInfo.push(floor_json)
         else
           @boolean = 0
           @floorInfo.each do |info|
             @damperType = key[2]
             if info.has_key?(key[2])
-              if info["floor"] == key[1]
-                info[key[2]] = value
+              if info["floor"] == key[1] || info["floor"] == key[3]
+                #info[key[2]] = value
                 @boolean = 1
               end
             end
+
           end
 
           if @boolean == 0
             floor_json["building"] = key[0]
              floor_data = key[1] == "other" ? key[3] : key[1]
              floor_json["floor"] = floor_data
-             #floor_json["floor"] = key[1]
-            if key[2] == "FSD"
-              floor_json["FSD"] = value
-              floor_json["FD"] = 0
-              floor_json["SD"] = 0
-            elsif key[2] == "FD"
-              floor_json["FSD"] = 0
-              floor_json["FD"] = value
-              floor_json["SD"] = 0
-            else
-              floor_json["FSD"] = 0
-              floor_json["FD"] = 0
-              floor_json["SD"] = value
-            end
-            if  key[1] == "other"
-             @building_result = Lsspdfasset.select(:u_building, :u_other_floor, :u_dr_passed_post_repair).where(:u_service_id => @owner.u_service_id, :u_building => @building, :u_other_floor => key[3], :u_delete => false).where.not(u_type: "").group(["u_building", "u_other_floor", "u_dr_passed_post_repair"]).count(:u_dr_passed_post_repair)
-          else
-            @building_result = Lsspdfasset.select(:u_building, :u_floor, :u_dr_passed_post_repair).where(:u_service_id => @owner.u_service_id, :u_building => @building, :u_floor => key[1], :u_delete => false).group(["u_building", "u_floor", "u_dr_passed_post_repair"]).count(:u_dr_passed_post_repair)
-           end
-            @building_result_len =  @building_result.length
-    
+            for_type_calculate_non_integer_floor_values(building_floors, building_other_floors, floor_data, floor_json, key, value)  
+            calculate_non_integer_floor_values(building_floors, building_other_floors, floor_data, key)
+
+
+
             @building_result.each do |fstatus, fvalue|
-             if !floor_json.has_key?(fstatus[2])
-               floor_json[fstatus[2]] = fvalue
-              end
+             if fstatus.class == String
+               if !floor_json.has_key?(fstatus)
+                 floor_json[fstatus] = fvalue
+               end
+             else
+               if !floor_json.has_key?(fstatus[2])
+                 floor_json[fstatus[2]] = fvalue
+               end
+	     end
             end
 
             if !floor_json.has_key?("Pass")
@@ -158,7 +129,6 @@ module Report
             if !floor_json.has_key?("NA")
               floor_json["NA"] = 0
             end            
-
             @floorInfo.push(floor_json)
           end
         end
@@ -219,5 +189,96 @@ module Report
        [DamperInspectionReporting.column_heading(:fail), $ftotal_damperPer],
        [DamperInspectionReporting.column_heading(:na), $natotal_damperPer]]
     end
+
+    def calculate_non_integer_floor_values(building_floors, building_other_floors, floor_data, key)
+      if building_floors & building_other_floors == []
+            if  key[1] == "other"
+              @building_result = Lsspdfasset.select(:u_building, :u_other_floor, :u_dr_passed_post_repair).where(:u_service_id => @owner.u_service_id, :u_building => @building, :u_other_floor => key[3], :u_delete => false).where.not(u_type: "").group(["u_building", "u_other_floor", "u_dr_passed_post_repair"]).count(:u_dr_passed_post_repair)
+            else
+              @building_result = Lsspdfasset.select(:u_building, :u_floor, :u_dr_passed_post_repair).where(:u_service_id => @owner.u_service_id, :u_building => @building, :u_floor => key[1], :u_delete => false).where.not(u_type: "").group(["u_building", "u_floor", "u_dr_passed_post_repair"]).count(:u_dr_passed_post_repair)
+            end
+          else
+            common_floors = building_floors & building_other_floors
+            if common_floors.include?(floor_data)
+            building_other_floor_result = Lsspdfasset.select(:u_building, :u_other_floor, :u_dr_passed_post_repair).where(:u_service_id => @owner.u_service_id, :u_building => @building, :u_other_floor => floor_data, :u_delete => false).where.not(u_type: "")
+                building_floor_result = Lsspdfasset.select(:u_building, :u_floor, :u_dr_passed_post_repair).where(:u_service_id => @owner.u_service_id, :u_building => @building, :u_floor => floor_data, :u_delete => false).where.not(u_type: "")
+               temp_result =  building_floor_result.as_json
+               temp_hash = {}
+               temp_hash[:id] = ""
+               temp_hash[:u_building] = building_other_floor_result.first.u_building
+               temp_hash[:u_floor] = building_other_floor_result.first.u_other_floor
+               temp_hash[:u_dr_passed_post_repair] = building_other_floor_result.first.u_dr_passed_post_repair
+               temp_result << temp_hash
+               @building_result = Hash[temp_result.group_by{|obj| obj["u_dr_passed_post_repair"] || obj[:u_dr_passed_post_repair]}.map{|k,v| [k,v.size]}]
+            else
+                @building_result = Lsspdfasset.select(:u_building, :u_floor, :u_dr_passed_post_repair).where(:u_service_id => @owner.u_service_id, :u_building => @building, :u_floor => key[1], :u_delete => false).where.not(u_type: "").group(["u_building", "u_floor", "u_dr_passed_post_repair"]).count(:u_dr_passed_post_repair)
+            end
+         end   
+    end	   
+
+   def for_type_calculate_non_integer_floor_values(building_floors, building_other_floors, floor_data, floor_json, key, value)
+      if building_floors & building_other_floors == []
+	  if key[2] == "FSD"
+            floor_json["FSD"] = value
+            floor_json["FD"] = 0
+            floor_json["SD"] = 0
+          elsif key[2] == "FD"
+            floor_json["FSD"] = 0
+            floor_json["FD"] = value
+            floor_json["SD"] = 0
+          else
+            floor_json["FSD"] = 0
+            floor_json["FD"] = 0
+            floor_json["SD"] = value
+          end
+          else
+            common_floors = building_floors & building_other_floors
+            if common_floors.include?(floor_data)
+
+            building_other_floor_result = Lsspdfasset.select(:u_building, :u_other_floor, :u_type).where(:u_service_id => @owner.u_service_id, :u_building => @building, :u_other_floor => floor_data, :u_delete => false).where.not(u_type: "")
+                building_floor_result = Lsspdfasset.select(:u_building, :u_floor, :u_type).where(:u_service_id => @owner.u_service_id, :u_building => @building, :u_floor => floor_data, :u_delete => false).where.not(u_type: "")
+               temp_result =  building_floor_result.as_json
+               temp_hash = {}
+               temp_hash[:id] = ""
+               temp_hash[:u_building] = building_other_floor_result.first.u_building
+               temp_hash[:u_floor] = building_other_floor_result.first.u_other_floor
+               temp_hash[:u_type] = building_other_floor_result.first.u_type
+               temp_result << temp_hash
+               @building_result = Hash[temp_result.group_by{|obj| obj["u_type"] || obj[:u_type]}.map{|k,v| [k,v.size]}]
+               @building_result.each do |fstatus, fvalue|
+		       
+                 if !floor_json.has_key?(fstatus)
+                   floor_json[fstatus] = fvalue
+		 end
+               end
+	        if !floor_json.has_key?("FSD")
+                   floor_json["FSD"] = 0
+                 end
+                 if !floor_json.has_key?("SD")
+                   floor_json["SD"] = 0
+                 end
+                 if !floor_json.has_key?("FD")
+                   floor_json["FD"] = 0
+                 end
+	       floor_json
+            else
+	       if key[2] == "FSD"
+            floor_json["FSD"] = value
+            floor_json["FD"] = 0
+            floor_json["SD"] = 0
+          elsif key[2] == "FD"
+            floor_json["FSD"] = 0
+            floor_json["FD"] = value
+            floor_json["SD"] = 0
+          else
+            floor_json["FSD"] = 0
+            floor_json["FD"] = 0
+            floor_json["SD"] = value
+          end
+              floor_json
+            end
+         end
+    end
+ 
   end
 end
