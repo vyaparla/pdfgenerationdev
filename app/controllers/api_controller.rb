558ae27144f98b0f653355f7b970707bf2ce6f95
Nisha <nganesh@vsoftconsulting.com>
2020-05-13 19:45:31 +0530

    Reverted the data file after updating all previous assets in lss qa

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -237,29 +237,23 @@ class ApiController < ApplicationController
       elsif params[:servicetype].delete(' ').upcase == "FIRESTOPSURVEY"
 
 	#To Be Removed
-	    path = Rails.root.to_s + "/testdata.csv"
+	    #path = Rails.root.to_s + "/testdata.csv"
 
-	    puts path
-  	    CSV.foreach(path) do |row|
+	    #puts path
+  	    #CSV.foreach(path) do |row|
 
-  	    puts row[0]
-            puts row[1]
-            puts row
-            find_asset =  Lsspdfasset.where(:u_asset_id => row[0]).first
-            find_asset.update(:u_updated_date => row[1]) if find_asset.present?
- # find_asset = Lsspdfasset.select("id, u_tag, u_asset_id, u_facility_name").where(:u_asset_id => ).first
-  #test_sql = "select id, u_tag, u_asset_id, u_facility_name from lsspdfassets where u_asset_id = 'd3157b16db1f4c50d17be1c2ca961975';"
-             puts "Running Queries"
-  #puts test_sql.gsub(/\n\s*/, ' ')
-            row_first = find_asset
-            if row_first.present?
-            puts row_first.id
-            puts row_first.u_tag
-            puts row_first.u_asset_id
-           puts row_first.u_facility_name
-           end
-           puts "After Execution"
-           end
+            #find_asset =  Lsspdfasset.where(:u_asset_id => row[0]).first
+            #find_asset.update(:u_updated_date => row[1]) if find_asset.present?
+            # puts "Running Queries"
+            #row_first = find_asset
+            #if row_first.present?
+            #puts row_first.id
+            #puts row_first.u_tag
+            #puts row_first.u_asset_id
+           #puts row_first.u_facility_name
+           #end
+           #puts "After Execution"
+           #end
 	#End      
         @records = Lsspdfasset.where(u_service_id: params[:serviceid], :u_delete => false).order("u_updated_date desc")
         p = Axlsx::Package.new
